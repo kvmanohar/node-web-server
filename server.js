43e0738d1cba672309bb8c7765a7e284821af00a
Manohar Kurapati <kvmanohar@gmail.com>
2018-08-09 22:17:35 +0100

    Added Project.html to the server

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ hbs.registerPartials(__dirname + "/views/partials");
 
 //register hbs helper functions
 hbs.registerHelper("getCurrentYear", () => {
-    return new Date().getFullYear();
+  return new Date().getFullYear();
 });
 
 hbs.registerHelper("screamIT", text => {
-    return text.toUpperCase();
+  return text.toUpperCase();
 });
 
 //set handlerbar (hbs) engine setting for template
@@ -27,15 +27,15 @@ app.use(express.static(__dirname + "/public"));
 
 //Middleware function to log the req and res
 app.use((req, res, next) => {
-    var now = new Date().toString();
-    var logMsg = now + ": " + req.method + " " + req.url;
-    console.log(logMsg);
-    fs.appendFile("server.log", logMsg + "\n", err => {
-        if (err) {
-            console.log("Unable to append to server.log.");
-        }
-    });
-    next();
+  var now = new Date().toString();
+  var logMsg = now + ": " + req.method + " " + req.url;
+  console.log(logMsg);
+  fs.appendFile("server.log", logMsg + "\n", err => {
+    if (err) {
+      console.log("Unable to append to server.log.");
+    }
+  });
+  next();
 });
 
 // // Maintenance page to be displayed all the time.
@@ -44,32 +44,39 @@ app.use((req, res, next) => {
 // });
 
 app.get("/", (req, res) => {
-    // res.send('<h1>Hello Express!</h1>');
-    // res.send({
-    //     name: "Manohar Kurapati",
-    //     likes: ["Coding", "Movies"]
-    // });
+  // res.send('<h1>Hello Express!</h1>');
+  // res.send({
+  //     name: "Manohar Kurapati",
+  //     likes: ["Coding", "Movies"]
+  // });
 
-    res.render("home.html", {
-        pageTitle: "WELCOME to Home Page",
-        welcomeMessage: "This is the home page of Node JS Home Tutorial."
-    });
+  res.render("home.html", {
+    pageTitle: "WELCOME to Home Page",
+    welcomeMessage: "This is the home page of Node JS Home Tutorial."
+  });
 });
 
 app.get("/about", (req, res) => {
-    // res.send("<h1>About Page!</h1>");
-    res.render("about.html", {
-        pageTitle: "About Page - Header passed",
-        welcomeMessage: "This is the About page of the website."
-    });
+  // res.send("<h1>About Page!</h1>");
+  res.render("about.html", {
+    pageTitle: "About Page - Header passed",
+    welcomeMessage: "This is the About page of the website."
+  });
+});
+
+app.get("/projects", (req, res) => {
+  res.render("projects.html", {
+    pageTitle: "Project Page",
+    welcomeMessage: "This is the project page welcome message."
+  });
 });
 
 app.get("/bad", (req, res) => {
-    res.send({
-        errorMsg: "Error page - Data not found",
-        errorCode: 501
-    });
+  res.send({
+    errorMsg: "Error page - Data not found",
+    errorCode: 501
+  });
 });
 app.listen(port, () => {
-    console.log(`Server is up on port ${port}`);
+  console.log(`Server is up on port ${port}`);
 });
